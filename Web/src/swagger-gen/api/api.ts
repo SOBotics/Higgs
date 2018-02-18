a5da3501a6bb57ca0fbd3693fdf7702177bd3e2b
Robert Rudman <rjrudman@gmail.com>
2018-02-18 18:50:45 +1100

    Add web component

---
+++ Web/src/swagger-gen/api/api.ts
@@ -0,0 +1,7 @@
+export * from './AdminApi';
+import { AdminApi } from './AdminApi';
+export * from './BotApi';
+import { BotApi } from './BotApi';
+export * from './ReviewerApi';
+import { ReviewerApi } from './ReviewerApi';
+export const APIS = [AdminApi, BotApi, ReviewerApi];
