5b134491dfece13b4de8712034a3bb414bd0e08c
Robert Rudman <rjrudman@gmail.com>
2018-03-03 18:00:38 +1100

    #6 - Hide feedback options when user isn't logged in.

--- Web/src/app/report/report.component.ts
+++ Web/src/app/report/report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TimeAgoPipe } from '../pipes/TimeAgoPipe';
 import { ReviewerService, ReviewerReportResponse } from '../../swagger-gen';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-report',
@@ -10,9 +11,18 @@ import { ReviewerService, ReviewerReportResponse } from '../../swagger-gen';
 })
 export class ReportComponent implements OnInit {
   public postDetails: ReviewerReportResponse;
-  constructor(private route: ActivatedRoute, private reviewerService: ReviewerService) { }
+  public isLoggedIn: boolean;
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private reviewerService: ReviewerService
+  ) { }
 
   ngOnInit() {
+    this.authService.GetAuthDetails().subscribe(details => {
+      this.isLoggedIn = details.IsAuthenticated;
+    });
+
     this.route.params.subscribe(params => {
       const reportId = +params['id'];
       this.reviewerService.reviewerReportGet(reportId).subscribe(response => {
