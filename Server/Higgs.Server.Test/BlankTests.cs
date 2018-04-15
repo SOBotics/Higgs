843cf66572483a1d6daf68e9bfe5b96ff5b6b504
Robert Rudman <rjrudman@gmail.com>
2018-04-15 23:40:34 +1000

    Add blank test for server for CI/CD

---
+++ Server/Higgs.Server.Test/BlankTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Higgs.Server.Test
+{
+    [TestFixture]
+    public class BlankTests
+    {
+        [Test]
+        public void BlankTest()
+        {
+            // Blank test to setup CI/CD
+        }
+    }
+}
