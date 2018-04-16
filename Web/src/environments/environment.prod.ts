1d49ec61dd598492ef6818cd2b1c361a099f61ef
Robert Rudman <rjrudman@gmail.com>
2018-04-16 18:41:05 +1000

    Update production environment settings

--- Web/src/environments/environment.prod.ts
+++ Web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: 'http://45.77.238.226',
-  webHost: 'http://45.77.238.226:5555',
+  apiHost: 'http://api.higgs.sobotics.org',
+  webHost: 'http://higgs.sobotics.org',
 };
