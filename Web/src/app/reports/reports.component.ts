4569db9530330973f123ff0e13603b41ed47b94c
Robert Rudman <rjrudman@gmail.com>
2018-06-12 13:34:57 +1000

    Implemented front end paging control

--- Web/src/app/reports/reports.component.ts
+++ Web/src/app/reports/reports.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReviewerService, PagingResponseReviewerReportsResponse } from '../../swagger-gen';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GetPagingInfo } from '../../utils/PagingHelper';
 
 @Component({
   selector: 'app-reports',
@@ -9,10 +11,24 @@ import { ReviewerService, PagingResponseReviewerReportsResponse } from '../../sw
 export class ReportsComponent implements OnInit {
 
   public reportsResponse: PagingResponseReviewerReportsResponse = null;
-  constructor(private reviewerService: ReviewerService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private reviewerService: ReviewerService) { }
 
   ngOnInit() {
-    this.reviewerService.reviewerReportsGet(1, 50)
-      .subscribe(response => this.reportsResponse = response);
+    this.route.queryParams.subscribe(params => {
+      const pageNumber = +params['pageNumber'] || 1;
+      this.reviewerService.reviewerReportsGet(pageNumber, 50)
+        .subscribe(response => this.reportsResponse = response);
+    });
+  }
+
+  public getPages() {
+    return GetPagingInfo(this.reportsResponse);
+  }
+
+  public loadPage(pageNumber: number) {
+    this.router.navigateByUrl(`/reports?pageNumber=${pageNumber}`);
   }
 }
---
+++ Web/src/utils/PagingHelper.ts
@@ -0,0 +1,78 @@
+export function GetPagingInfo(pagedData: { pageNumber?: number, totalPages?: number }, numExtraPages = 2) {
+    if (!pagedData) {
+        return [];
+    }
+    const totalPages = pagedData.totalPages || 0;
+    if (totalPages === 0) {
+        return [];
+    }
+    const pageNumber = pagedData.pageNumber || 1;
+    const pages = [];
+
+    // Always add in 'Previous' button. Disabled if we're on page one (or less, for defensive coding)
+    pages.push({
+        Name: 'Previous',
+        Number: pageNumber - 1,
+        Active: false,
+        Disabled: pageNumber <= 1
+    });
+
+    if (pageNumber - numExtraPages > 1) {
+        // Add in page 1 if we're far away
+        pages.push({
+            Name: '1',
+            Number: 1,
+            Active: false,
+            Disabled: false
+        });
+
+        // Add in dots if we have a missing number between 1 and our first showing number
+        if (pageNumber - numExtraPages > 2) {
+            pages.push({
+                Name: '...',
+                Number: -1,
+                Active: false,
+                Disabled: true
+            });
+        }
+    }
+
+    // Add in pages before and after our selected page
+    for (let i = pageNumber - numExtraPages; i <= pageNumber + numExtraPages; i++) {
+        if (i <= 0 || i > totalPages) {
+            continue;
+        }
+        pages.push({ Name: i + '', Number: i, Active: i === pageNumber });
+    }
+
+
+    if (totalPages - pageNumber - numExtraPages > 0) {
+        // Add in dots if we have a missing number between our last showing number and the last page
+        if (totalPages - pageNumber - numExtraPages > 1) {
+            pages.push({
+                Name: '...',
+                Number: -1,
+                Active: false,
+                Disabled: true
+            });
+        }
+
+        // Add in page 1 if we're far away
+        pages.push({
+            Name: totalPages + '',
+            Number: totalPages,
+            Active: false,
+            Disabled: false
+        });
+    }
+
+    // Always add in 'Next' button. Disabled if we're on the last page (or above)
+    pages.push({
+        Name: 'Next',
+        Number: pageNumber + 1,
+        Active: false,
+        Disabled: pageNumber >= totalPages
+    });
+
+    return pages;
+}
