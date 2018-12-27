37e6bf5298966293dcc55d437361b3f252f5e942
Robert Rudman <rjrudman@gmail.com>
2018-12-28 10:20:37 +1100

    Fix authentication on pages with query strings

--- Server/Higgs.Server/Controllers/AuthenticationController.cs
+++ Server/Higgs.Server/Controllers/AuthenticationController.cs
@@ -7,6 +7,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using Higgs.Server.Data;
 using Higgs.Server.Utilities;
 using Microsoft.ApplicationInsights.AspNetCore.Extensions;
@@ -147,7 +148,13 @@ namespace Higgs.Server.Controllers
 
             var token = CreateJwtToken(claims, signingKey);
 
-            return Redirect($"{loginState.RedirectURI}?access_token={token}");
+            var uriBuilder = new UriBuilder(loginState.RedirectUri);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["access_token"] = token;
+            uriBuilder.Query = query.ToString();
+            var redirectUrl = uriBuilder.ToString();
+
+            return Redirect($"{redirectUrl}?access_token={token}");
         }
 
         private byte[] GetSigningKey()
